ac9ab80caa565cbb63ba08669e8ca138ca0631ad
Gerrit Hall <gerrit@rezscore.com>
2021-08-07 19:53:44 -0700

    feat: Script to mint one NFT per Senator

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,4 +4,5 @@ from brownie import *
 def main():
     deploy_acct = accounts.load("ropsten")
     mc = MockCongress.deploy(accounts[0], {"from": accounts[0]}, publish_source=True)
+    mc.set_alias("MOC")
     return mc
